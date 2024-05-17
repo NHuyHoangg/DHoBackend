ff0487aac6cc30d7d99a9a9f23d7c915da4f93fa
NHuyHoangg <80319763+NHuyHoangg@users.noreply.github.com>
2024-05-17 16:13:32 +0700

    Update logger.js

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -9,16 +9,28 @@ async function logMiddleware(req, res, next) {
   const api_route = req.originalUrl;
   const server_domain = req.hostname;
   const startTime = new Date();
+  let user_id;
+  // console.log(res)
   res.on("finish", async () => {
-    const user_id = req.user.id || "unknown";
+    if (req.user === undefined) {
+      user_id = "unknown";
+    } else user_id = req.user.id;
+
     const body = JSON.stringify(req.body);
     const endTime = new Date();
     const responseTime = endTime - startTime;
-      const client = await pool.connect();
-
-    client.query(
+    pool.query(
       "INSERT INTO logs (level, message, user_id, api_route, server_domain,ip,time_now,msg) VALUES ($1, $2, $3, $4,$5 ,$6,$7,$8)",
-      [body, responseTime, user_id, api_route, server_domain, ip, vietnamTime,res],
+      [
+        body,
+        responseTime,
+        user_id,
+        api_route,
+        server_domain,
+        ip,
+        vietnamTime,
+        res.error,
+      ],
       (err, result) => {
         if (err) {
           console.error("Error inserting log:", err.message);
